fb2283e40f2d51713225c5065e353d6e8b32b2a9
hasa0017 <syedtahirh@yahoo.com>
2015-12-28 01:43:46 +0500

    change 2
    added code for function makecachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -4,9 +4,16 @@
 ## functions do
 
 ## Write a short comment describing this function
+## 
 
-makeCacheMatrix <- function(x = matrix()) {
 
+  makeCacheMatrix <- function(x = matrix()) {
+    m<-NULL
+    set<-function(y){
+      x<<-y
+      m<<-NULL
+    }
+    
 }
 
 
